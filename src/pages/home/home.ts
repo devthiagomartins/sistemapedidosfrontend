aedcf4f2d17895c0cad6d3a36e3241a90176434e
Thiago Martins Reis <devthiagomartins@gmail.com>
2023-01-14 09:38:28 -0300

    criando pag de Signup

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, MenuController, NavController, ToastController } from 'ionic-angular';
+import { IonicPage, MenuController, NavController } from 'ionic-angular';
 import { CredenciaisDTO } from '../../models/credenciais.dto';
 import { AuthService } from '../../services/auth.service';
 
@@ -14,7 +14,7 @@ export class HomePage {
     email : "",
     senha : ""
   }
-  constructor(public navCtrl: NavController, public menu: MenuController, public auth: AuthService, public toastCtrl : ToastController) {
+  constructor(public navCtrl: NavController, public menu: MenuController, public auth: AuthService) {
 
   }
 
@@ -36,17 +36,12 @@ export class HomePage {
     this.auth.authenticate(this.creds).subscribe(response => {
       this.auth.successfulLogin(response.headers.get('Authorization'));
       this.navCtrl.setRoot('CategoriasPage');
-      this.presentToast();
     }, error => {});   
   }
 
-  presentToast() {
-    const toast = this.toastCtrl.create({
-      message: 'Logado com sucesso!',
-      duration: 3000,
-      position: 'top',
-    });
-    toast.present();
+  signup() {
+    console.log('passou');
+    this.navCtrl.push('SignupPage');
   }
 
 }
