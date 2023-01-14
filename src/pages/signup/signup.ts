5dfd0a2345c4b9a1b4708b58b4d08bff18133146
Thiago Martins Reis <devthiagomartins@gmail.com>
2023-01-14 11:06:58 -0300

    povoando cidade e estado dinamicamente

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { CidadeDTO } from '../../models/cidade.dto';
+import { EstadoDTO } from '../../models/estado.dto';
+import { CidadeService } from '../../services/domain/cidade.service';
+import { EstadoService } from '../../services/domain/estado.service';
 
 @IonicPage()
 @Component({
@@ -10,7 +14,16 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class SignupPage {
 
   formGroup : FormGroup;
-  constructor(public navCtrl: NavController, public navParams: NavParams, public formBuilder: FormBuilder) {
+  estados: EstadoDTO[];
+  cidades: CidadeDTO[];
+
+  constructor(public navCtrl: NavController, 
+              public navParams: NavParams, 
+              public formBuilder: FormBuilder, 
+              public cidadeService: CidadeService, 
+              public estadoService: EstadoService) {
+
+            
     this.formGroup = this.formBuilder.group({
       nome: ['ThiagoRemover',[Validators.required,
                  Validators.minLength(3), 
@@ -48,7 +61,23 @@ export class SignupPage {
     });
   }
 
+  ionViewDidLoad() {
+    this.estadoService.findAll().subscribe(response => {
+      this.estados = response;
+      this.formGroup.controls.estadoId.setValue(this.estados[0].id);
+      this.updateCidades();
+      
+    }, error => {});
+  }
   signupUser() {
     console.log('enviou o form');
+  }
+
+  updateCidades() {
+    let estado_id = this.formGroup.value.estadoId;
+    this.cidadeService.findAll(estado_id).subscribe(response => {
+      this.cidades = response;
+      this.formGroup.controls.cidadeId.setValue(null);
+    }, error => {});
   }
 }
