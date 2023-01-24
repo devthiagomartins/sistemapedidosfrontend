9383129e0d4b16d7bb9427b3e1ce9fe89cb35821
Thiago Martins Reis <devthiagomartins@gmail.com>
2023-01-23 23:10:29 -0300

    fazendo upload da foto

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -22,6 +22,10 @@ export class ProfilePage {
   }
 
   ionViewDidLoad() {
+   this.loadData();
+  }
+
+  loadData() {
     let localUser = this.storage.getLocalUser();
     if(localUser && localUser.email){
       this.clienteService.findByEmail(localUser.email).subscribe(response => {
@@ -59,6 +63,17 @@ export class ProfilePage {
      this.cameraOn = false;
     }, (err) => {
     });
+  }
+
+  sendPicture() {
+    this.clienteService.uploadPicture(this.picture).subscribe(response => {
+      this.picture = null;
+      this.loadData();
+    }, error => {});
+  }
+
+  cancel() {
+    this.picture = null;
   }
 
 }
